dab2be54d3156c3ebdb39dc78d9627a8ba1d30c2
Torcato Lopes <tdlopes95@gmail.com>
2026-03-16 19:02:06 +0000

    new: Added printwriter function to save to a .txt

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import java.io.PrintWriter;
 
 public class Main {
     static String titulo = "";
+    static PrintWriter escrita = null;
 
     public static void main(String[] args) throws FileNotFoundException {
         String validador="";
@@ -15,9 +16,13 @@ public class Main {
 
         while(!validador.equalsIgnoreCase("N")){
 
-            System.out.println("Insira o nome do moodmap que deseja ler: (ex:mood_nov.txt");
+            System.out.println("Insira o nome do moodmap que deseja ler: (ex:mood_nov.txt)");
             mood= ler.nextLine();
             int[][] moodData = lerMood(mood);           // a)
+
+            //printwriter
+            String nomeFicheiro = mood.replace(".txt", "_analysis.txt");
+            escrita = new PrintWriter(new File("output/" + nomeFicheiro));
 
             visualizarMoodMap(moodData);                // b)
             double[] mediaDia = mediaPorDia(moodData);  // c)
@@ -29,6 +34,8 @@ public class Main {
             pessoasTerapia(moodData, maxConsecutivoPorPessoa);                   // i)
             humorSemelhante(moodData);                  // j)
 
+            escrita.close();
+            System.out.printf("Analysis saved to output/%s\n", nomeFicheiro);
 
             System.out.println("Deseja Continuar? (S/N)");
             String repMain = ler.next();
@@ -71,11 +78,13 @@ public class Main {
         }
 
         System.out.print("\nj) People with the most similar moods: ");
+        escrita.print("\nj) People with the most similar moods: ");
         if (maxIguais == 0) {
             System.out.println("there aren't people with similar moods.");
+            escrita.println("there aren't people with similar moods.");
         } else {
-            System.out.printf("(Person #%d and Person #%d have the same mood on %d days)\n",
-                    pessoa_1 + 1, pessoa_2 + 1, maxIguais);
+            System.out.printf("(Person #%d and Person #%d have the same mood on %d days)\n", pessoa_1 + 1, pessoa_2 + 1, maxIguais);
+            escrita.printf("(Person #%d and Person #%d have the same mood on %d days)\n", pessoa_1 + 1, pessoa_2 + 1, maxIguais);
         }
     }
     /*****************************************************
@@ -86,19 +95,23 @@ public class Main {
         boolean validador = false;
 
         System.out.println("i) Recommended therapy:");
+        escrita.println("i) Recommended therapy:");
 
         for (int i = 0; i < pessoas; i++) {
             if (maxConsecutivoPorPessoa[i] >= 2 && maxConsecutivoPorPessoa[i] <= 4) {
                 System.out.printf("Person #%d : Listen to music\n", i + 1);
+                escrita.printf("Person #%d : Listen to music\n", i + 1);
                 validador = true;
             } else if (maxConsecutivoPorPessoa[i] >= 5) {
                 System.out.printf("Person #%d : Psychological support\n", i + 1);
+                escrita.printf("Person #%d : Psychological support\n", i + 1);
                 validador = true;
             }
         }
 
         if (!validador) {
             System.out.println("Not needed. You have a happy business!");
+            escrita.println("Not needed. You have a happy business!");
         }
     }
     /*****************************************************
@@ -109,6 +122,7 @@ public class Main {
         int dias = moodData[0].length;
 
         System.out.println("\nh) People's Mood Level Charts:");
+        escrita.println("\nh) People's Mood Level Charts:");
 
         for (int i = 0; i < pessoas; i++) {
             // verificacao dos limites para uma pessoa
@@ -124,35 +138,46 @@ public class Main {
             }
 
             System.out.printf("Person #%d:%n", i + 1);
+            escrita.printf("Person #%d:%n", i + 1);
 
             // impressao
             for (int mood = maxMood; mood >= minMood; mood--) {
                 System.out.printf("%4d |", mood);
+                escrita.printf("%4d |", mood);
                 for (int j = 0; j < dias; j++) {
                     if (moodData[i][j] == mood) {
                         System.out.print("*");
+                        escrita.print("*");
                     } else {
                         System.out.print(" ");
+                        escrita.print(" ");
                     }
                 }
                 System.out.println();
+                escrita.println();
             }
 
             // layout de baixo
             System.out.print("Mood +");
+            escrita.print("Mood +");
             for (int j = 0; j < dias; j++) {
                 System.out.print("-");
+                escrita.print("-");
             }
             System.out.println();
+            escrita.println();
 
             // dias
             System.out.print("     ");
+            escrita.print("     ");
             for (int j = 0; j <= dias; j++) {
                 if (j % 5 == 0) {
                     System.out.printf("%-5d", j);
+                    escrita.printf("%-5d", j);
                 }
             }
             System.out.println("\n");
+            escrita.println("\n");
         }
     }
     /*****************************************************
@@ -165,6 +190,7 @@ public class Main {
         int[] maxConsecutivoPorPessoa = new int[pessoas];
 
         System.out.println("\ng) People with emotional disorders:");
+        escrita.println("\ng) People with emotional disorders:");
 
         for (int i = 0; i < pessoas; i++) {
             int maxConsecutivo = 0;
@@ -185,6 +211,7 @@ public class Main {
 
             if (maxConsecutivo >= 2) {
                 System.out.printf("Person #%d : %d consecutive days%n", i+1, maxConsecutivo);
+                escrita.printf("Person #%d : %d consecutive days%n", i+1, maxConsecutivo);
                 validador = true;
             }
 
@@ -192,6 +219,7 @@ public class Main {
 
         if (!validador) {
             System.out.println("No one! You have a happy company!");
+            escrita.println("No one! You have a happy company!");
         }
         return maxConsecutivoPorPessoa;
     }
@@ -213,9 +241,11 @@ public class Main {
         }
 
         System.out.println("\nf) Percentage of mood levels:");
+        escrita.println("\nf) Percentage of mood levels:");
         for (int mood = 4; mood >= 0; mood--) {
             percentagem = (double) contador[mood] / total * 100;
             System.out.printf("Mood #%d: %.1f%%%n", mood+1, percentagem);
+            escrita.printf("Mood #%d: %.1f%%%n", mood+1, percentagem);
         }
     }
     /*****************************************************
@@ -232,13 +262,16 @@ public class Main {
         }
 
         System.out.printf("\ne) Days with the highest average mood (%.1f)  :  ", maior);
+        escrita.printf("\ne) Days with the highest average mood (%.1f)  :  ", maior);
         // teste para ver se há mais dias com a maior média
         for (int i = 0; i < dias; i++) {
             if (mediaDia[i] == maior) {
                 System.out.printf("%d ", i);
+                escrita.printf("%d ", i);
             }
         }
         System.out.println();
+        escrita.println();
     }
     /*****************************************************
      ************** d) Média por pessoa ******************
@@ -249,6 +282,7 @@ public class Main {
         double media;
 
         System.out.println("\nd) Average of each person's mood: ");
+        escrita.println("\nd) Average of each person's mood: ");
         for (int i = 0; i < pessoas; i++) {
             int soma = 0;
 
@@ -257,6 +291,7 @@ public class Main {
             }
             media = (double) soma / dias;
             System.out.printf("Person #%-3d: %.1f%n", i+1, media);
+            escrita.printf("Person #%-3d: %.1f%n", i+1, media);
         }
     }
 
@@ -278,25 +313,35 @@ public class Main {
         }
 
         System.out.println("\nc) Average mood each day:");
+        escrita.println("\nc) Average mood each day:");
         System.out.printf("%-11s:", "day"); //cabeçalho
+        escrita.printf("%-11s:", "day");
         for (int j = 0; j < dias; j++) {
-            System.out.printf("%4d", j);
+            System.out.printf("%4d", j+1);
+            escrita.printf("%4d", j+1);
         }
         System.out.println();
+        escrita.println();
 
 
         System.out.print("-----------|"); //separador
+        escrita.print("-----------|");
         for (int j = 0; j < dias; j++) {
             System.out.print("---|");
+            escrita.print("---|");
         }
         System.out.println();
+        escrita.println();
 
 
         System.out.printf("%-12s", "mood"); //medias
+        escrita.printf("%-12s", "mood");
         for (int j = 0; j < dias; j++) {
             System.out.printf("%4.1f", mediaDiaria[j]);
+            escrita.printf("%4.1f", mediaDiaria[j]);
         }
         System.out.println();
+        escrita.println();
 
         return mediaDiaria;
     }
@@ -309,26 +354,37 @@ public class Main {
         int dias = moodData[0].length;
 
         System.out.printf("\n %s \n",titulo); //titulo
+        escrita.printf("\n %s \n",titulo);
         System.out.println("b) Mood (level/day(person)");
+        escrita.println("b) Mood (level/day(person)");
 
         System.out.printf("%-11s:", "day"); //cabeçalho
+        escrita.printf("%-11s:", "day");
         for (int j = 0; j < dias; j++) {
             System.out.printf("%4d", j+1);
+            escrita.printf("%4d", j+1);
         }
         System.out.println();
+        escrita.println();
 
         System.out.print("-----------|"); //separador superior
+        escrita.print("-----------|");
         for (int j = 0; j < dias; j++) {
             System.out.print("---|");
+            escrita.print("---|");
         }
         System.out.println();
+        escrita.println();
 
         for (int i = 0; i < pessoas; i++) {
             System.out.printf("Person #%-3d:", i+1);
+            escrita.printf("Person #%-3d:", i+1);
             for (int j = 0; j < dias; j++) {
                 System.out.printf("%4d", moodData[i][j]);
+                escrita.printf("%4d", moodData[i][j]);
             }
             System.out.println();
+            escrita.println();
         }
 
     }
